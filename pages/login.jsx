3b9e2497096828422144961ae7e20c08d65848fd
Mohsin <mohsincse2015@gmail.com>
2023-01-10 17:20:20 +0600

    sth fix

--- pages/login.jsx
+++ pages/login.jsx
@@ -3,47 +3,47 @@ import React from "react";
 function login() {
   return (
     <div className="container py-10 px-40">
-      <div class="p-6 bg-[#F6F6F6] rounded-xl shadow-lg">
+      <div className="p-6 bg-[#F6F6F6] rounded-xl shadow-lg">
         <p className="mx-auto text-2xl text-center mb-8">
           Login To Your Account
         </p>
         <div className="flex justify-between">
-          <div class="absolute left-1/2 -ml-0.5 w-px h-44 mt-10 bg-gray-600"></div>
+          <div className="absolute left-1/2 -ml-0.5 w-px h-44 mt-10 bg-gray-600"></div>
 
           <div>
             <p className="mb-2">Login or Register with Mobile Number</p>
             <div className="mb-4 w-96 mx-auto">
-              <div class="relative mt-1 rounded-md shadow-sm">
-                <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+              <div className="relative mt-1 rounded-md shadow-sm">
+                <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
                   <div className="flex">
-                    <span class="text-gray-500">
+                    <span className="text-gray-500">
                       <img src="/bd_flag.png" />{" "}
                     </span>
-                    <span class="text-gray-500 ml-1"> +88</span>
+                    <span className="text-gray-500 ml-1"> +88</span>
                   </div>
                 </div>
                 <input
                   type="text"
-                  class="block shadow appearance-none border rounded w-full py-2 pl-20 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  className="block shadow appearance-none border rounded w-full py-2 pl-20 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   placeholder="01*********"
                 />
               </div>
             </div>
 
             <div className="mb-6 w-96 mx-auto">
-              <button class="rounded bg-black text-white w-full py-2 px-3">
+              <button className="rounded bg-black text-white w-full py-2 px-3">
                 Login with OTP
               </button>
             </div>
 
             <p className="mb-1">Or Login With</p>
 
-            <div class="flex justify-between">
+            <div className="flex justify-between">
               <a
                 href="#"
-                class="px-6 py-3 text-white no-underline bg-[#3B5998] rounded w-36"
+                className="px-6 py-3 text-white no-underline bg-[#3B5998] rounded w-36"
               >
-                <div class="flex justify-between">
+                <div className="flex justify-between">
                   <img src="/fb_icon.png" />
                   <span>Facebook</span>
                 </div>
@@ -51,9 +51,9 @@ function login() {
 
               <a
                 href="#"
-                class="px-6 py-3 no-underline bg-[#ffffff] rounded w-36"
+                className="px-6 py-3 no-underline bg-[#ffffff] rounded w-36"
               >
-                <div class="flex justify-between">
+                <div className="flex justify-between">
                   <img src="/google_icon.png" />
                   <span>Google</span>
                 </div>
@@ -65,17 +65,17 @@ function login() {
             <form action="#">
               <p className="mb-2">Login with Phone Number or Email</p>
 
-              <div class="mb-4 w-96 mx-auto">
+              <div className="mb-4 w-96 mx-auto">
                 <input
-                  class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   type="text"
                   placeholder="Email or Phone"
                 />
               </div>
 
-              <div class="w-96 mx-auto">
+              <div className="w-96 mx-auto">
                 <input
-                  class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   type="password"
                   placeholder="Password"
                 />
@@ -86,7 +86,7 @@ function login() {
               </a>
 
               <div className="mb-2 w-96 mx-auto">
-                <button class="rounded bg-black text-white w-full py-2 px-3">
+                <button className="rounded bg-black text-white w-full py-2 px-3">
                   Login
                 </button>
               </div>
