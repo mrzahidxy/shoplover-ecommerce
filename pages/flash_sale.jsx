0bb8a4f34864e227b4ebe9f33aeb4e4f7b27a0b1
Mohsin <mohsincse2015@gmail.com>
2023-01-19 15:07:14 +0600

    flash sale page

---
+++ pages/flash_sale.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+const flashSales = [
+  { id: 1, name: "Books Collection" },
+  { id: 2, name: "Books Collection" },
+  { id: 3, name: "Books Collection" },
+  { id: 4, name: "Books Collection" },
+  { id: 5, name: "Books Collection" },
+  { id: 6, name: "Books Collection" },
+  { id: 7, name: "Books Collection" },
+  { id: 8, name: "Books Collection" },
+  { id: 9, name: "Books Collection" },
+  { id: 10, name: "Books Collection" },
+  { id: 11, name: "Books Collection" },
+  { id: 12, name: "Books Collection" },
+];
+
+function Flash_sale() {
+  return (
+    <div className="container mb-5">
+      <div className="mt-5">
+        <img src="/flash_sale_banar.png" />
+      </div>
+
+      <div className="mt-4"></div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {flashSales.map((flashSale, index) => (
+          <div className=" border boreder-secondary" key={index}>
+            <div className="">
+              <img src="/flash_sale_banar.png" className="" />
+            </div>
+            <div className="flex justify-between mt-1">
+              <div className="text-base font-semibold">{flashSale.name}</div>
+              <div className="">
+                <span className="bg-black text-white text-xs px-1 py-[2px] rounded-sm">
+                  03
+                </span>
+                :
+                <span className="bg-black text-white text-xs px-1 py-[2px] rounded-sm">
+                  16
+                </span>
+                :
+                <span className="bg-black text-white text-xs px-1 py-[2px] rounded-sm">
+                  27
+                </span>
+                :
+                <span className="bg-black text-white text-xs px-1 py-[2px] rounded-sm">
+                  51
+                </span>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Flash_sale;
