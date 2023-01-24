80f9fd700c516078d6cfd03a941e24c4b72f8d43
Rakibul Alam Nahin <ranahin648@gmail.com>
2023-01-24 16:54:29 +0600

    cleaned the create shipping address

--- pages/users/createShippingAddress.js
+++ pages/users/createShippingAddress.js
@@ -14,7 +14,7 @@ const createShippingAddress = () => {
 
     return (
         <div className='mt-10 p-4 lg:w-285 bg-white sm:max-lg:flex sm:max-lg:flex-col sm:max-lg:items-center'>
-            <div className=' lg:border-b-1'><PageName name={"Create new shipping address"} /></div>
+            <div className='lg:pl-8 sm:max-lg:self-start lg:border-b-1'><PageName name={"Create new shipping address"} /></div>
 
             <div className='lg:m-8 w-full max-w-2xl lg:w-96  shadow-myshadow flex flex-col justify-between rounded-md' >
                 <span className='px-3 h-14 flex flex-row justify-between items-center border-b-1 '>
@@ -54,16 +54,16 @@ const createShippingAddress = () => {
             </div>
 
             <div className='mt-6 lg:mx-8 p-5 w-full max-w-2xl lg:w-96  shadow-myshadow flex flex-col justify-between rounded-md' >
-                <span className='font-medium mb-4'>Address Type</span>
+                <span className='font-semibold mb-4'>Address Type</span>
                 <div>
                     <button className='w-24 h-10 rounded-md mr-3 border-1 border-black text-slate-300 font-thin text-sm hover:bg-black hover:text-white focus:bg-black focus:text-white'>Home</button>
                     <button className='w-24 h-10 rounded-md mr-3 border-1 border-black text-slate-300 font-thin text-sm hover:bg-black hover:text-white focus:bg-black focus:text-white'>Office</button>
-                    <button className='w-24 h-10 rounded-md mr-3 border-1 border-black text-slate-300 font-thin text-sm hover:bg-black hover:text-white focus:bg-black focus:text-white'>Other</button>
+                    <button className='w-24 h-10 rounded-md  border-1 border-black text-slate-300 font-thin text-sm hover:bg-black hover:text-white focus:bg-black focus:text-white'>Other</button>
 
                 </div>
             </div>
 
-            <div className='mt-3 lg:mx-8 p-5 w-full max-w-2xl lg:w-96 flex flex-row justify-between' >
+            <div className='mt-3 lg:mx-8 py-4 w-full max-w-2xl lg:w-96 flex flex-row justify-between' >
                 <span>Make default shipping address</span>
                 <button className='w-14 h-6 flex flex-row items-center bg-slate-400 rounded-xl'
                     onClick={()=>{
