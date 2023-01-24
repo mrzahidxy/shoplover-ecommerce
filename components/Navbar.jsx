d099a43b8b35e56f91a6aa19e0b378af4e4cc17e
Zahid <mrzahidxy@gmail.com>
2023-01-24 16:58:27 +0600

    navbar category list

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,84 +1,111 @@
-import {Dropdown } from "antd";
+import { Dropdown } from "antd";
 import Link from "next/link";
+import SubCategory from "./SubCategory";
 
 const Navbar = () => {
   const nav = [
     { id: 1, name: "Home", link: "/" },
     { id: 2, name: "Winter Sale", link: "wintersale" },
-    { id: 3, name: "Flash Sale", link: "/flashsale" },
+    { id: 3, name: "Flash Sale", link: "/flash_sale" },
     { id: 4, name: "Blogs", link: "/blogs" },
     { id: 5, name: "Order Track", link: "/cart" },
   ];
 
-  const cardItem = () => {
-    return (
-      <div>
-        <div>Hello</div>
-        <div>Hello</div>
-        <div>Hello</div>
-        <div>Hello</div>
-      </div>
-    );
-  };
-
   const items = [
     {
       key: "1",
-      label: "Menu",
+      label: (
+        <button className="font-mdeium">
+          <span> Women’s Lifestyle </span>
+        </button>
+      ),
       children: [
         {
-          key: "3",
-          label:( <span>
-            <cardItem/>
-          </span>),
+          key: "1-1",
+          label: (
+            <span>
+              <SubCategory />
+            </span>
+          ),
+        },
+      ],
+    },
+
+    {
+      key: "2",
+      label: (
+        <button className="font-mdeium">
+          <span> Men’s Lifestyle </span>
+        </button>
+      ),
+      children: [
+        {
+          key: "2-1",
+          label: (
+            <span>
+              <SubCategory />
+            </span>
+          ),
         },
       ],
     },
 
     {
       key: "3",
-      label: "Menu",
-    },
-
-    {
-      key: "1",
-      label: "Menu",
+      label: "Consumer Electronics",
     },
     {
-      key: "2",
-      label: "Menu",
+      key: "4",
+      label: "Mobile & Device",
     },
     {
-      label: "Menu",
-    },
-
-    {
-      key: "1",
-      label: "Menu",
+      key: "5",
+      label: "Grocery & Food",
     },
     {
-      key: "2",
-      label: "Menu",
+      key: "6",
+      label: "Mother, Kids & Toys",
     },
     {
-      key: "3",
-      label: "Menu",
+      key: "7",
+      label: "IT Products",
+    },
+    {
+      key: "8",
+      label: "Health & Beautye",
+    },
+    {
+      key: "9",
+      label: "Home & Living",
+    },
+    {
+      key: "10",
+      label: "Digital Goods",
+    },
+    {
+      key: "11",
+      label: "Sports & Accessories",
+    },
+    {
+      key: "12",
+      label: "Digital Goods",
     },
   ];
   return (
     <div className="bg-[#1b1b1bf7] text-white h-11  hidden lg:block">
       <div className="h-full mx-24 flex justify-between items-center">
-        <div className="font-base font-medium flex gap-2 ">
-          <img src="/category-icon.svg" alt="" />
-          <Dropdown
-            menu={{
-              items,
-            }}
-            placement="rightBottom"
-          >
+        <Dropdown
+          arrow="false"
+          menu={{ items, style: { margingBottom: "20px" } }}
+          trigger={["click"]}
+          placement="rightBottom"
+        >
+          <div className="font-base font-medium flex gap-2 ">
+            <img src="/category-icon.svg" alt="" />
+
             <span>All Categories</span>
-          </Dropdown>
-        </div>
+          </div>
+        </Dropdown>
         <nav className="flex gap-7">
           {nav.map((n) => (
             <Link
