2281559691723e89d494d03f22634515c7221a03
Zahid <mrzahidxy@gmail.com>
2023-01-26 15:32:02 +0600

    fix: slide css issues

--- components/ProductSlider.jsx
+++ components/ProductSlider.jsx
@@ -2,6 +2,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Product from "./Product";
+import Link from "next/link";
 
 const products = [
   {
@@ -81,21 +82,21 @@ const products = [
 const settings = {
   infinite: true,
   speed: 500,
-  slidesToShow: 6,
+  slidesToShow: 5,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 1000,
   pauseOnHover: true,
-  // nextArrow: (
-  //   <div>
-  //     <img src="/next-icon.svg" alt="" />
-  //   </div>
-  // ),
-  // prevArrow: (
-  //   <div>
-  //     <img src="/prev-icon.svg" alt="" />
-  //   </div>
-  // ),
+  nextArrow: (
+    <div>
+      <img src="/next-icon.svg" alt="" />
+    </div>
+  ),
+  prevArrow: (
+    <div>
+      <img src="/prev-icon.svg" alt="" />
+    </div>
+  ),
   responsive: [
     {
       breakpoint: 745,
@@ -119,9 +120,38 @@ const settings = {
 
 const ProductSlider = () => {
   return (
-    <Slider {...settings} >
+    <Slider {...settings}>
       {products.map((p) => (
-        <Product product={p} key={p.id}/>
+        <div
+          href={`/products/${p.id}`}
+          className="border border-color-[#E3E3E3] rounded-md"
+          key={Math.random()}
+        >
+          <div className="h-[164px] md:h-[200px]  lg:h-[230px] bg-[#FAFAFA] flex items-center justify-center">
+            <img src={p.img} />
+          </div>
+          <div className="flex flex-col gap-2 p-3">
+            <span className="w-[69px] bg-[#FFEBE6] text-xs font-semibold text-[#B92500] flex justify-center ">
+              Sale-12%
+            </span>
+            <span className="text-xs md:text-sm lg:text-base font-normal">
+              {p.title}
+            </span>
+            <div className="flex ">
+              <img src="/star-icon.svg" />
+              <img src="/star-icon.svg" />
+              <img src="/star-icon.svg" />
+              <img src="/star-icon.svg" />
+              <span className="text-[10px] font-[#7A869A]">(10)</span>
+            </div>
+            <div className="flex gap-1 items-baseline">
+              <span className="text-sm md:text-lg lg:text-xl">{p.price}</span>{" "}
+              <span className="text-xs font-[#7A869A] line-through">
+                ৳1,86,400
+              </span>
+            </div>
+          </div>
+        </div>
       ))}
     </Slider>
   );
