236e04135502d5decbd5751e934024dc4ac61cb6
Mohsin <mohsincse2015@gmail.com>
2023-01-30 10:25:36 +0600

    count flash sale

--- pages/flash_sale.jsx
+++ pages/flash_sale.jsx
@@ -1,5 +1,13 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Link from "next/link";
+
+const STATUS = {
+  STARTED: "Started",
+  STOPPED: "Stopped",
+};
+
+// const INITIAL_COUNT = 11; // 11sec
+const INITIAL_COUNT = 172811; // 2days 11sec
 
 const flashSales = [
   { id: 1, name: "Books Collection" },
@@ -16,7 +24,47 @@ const flashSales = [
   { id: 12, name: "Books Collection" },
 ];
 
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
+
+const twoDigits = (num) => String(num).padStart(2, "0");
+
 function Flash_sale() {
+  const [secondsRemaining, setSecondsRemaining] = useState(INITIAL_COUNT);
+  const [status, setStatus] = useState(STATUS.STARTED);
+
+  const secondsToDisplay = secondsRemaining % 60;
+  const minutesRemaining = (secondsRemaining - secondsToDisplay) / 60;
+  const minutesToDisplay = minutesRemaining % 60;
+  const hoursRemaining = (minutesRemaining - minutesToDisplay) / 60;
+  const hoursToDisplay = hoursRemaining % 24;
+  const daysToDisplay = (hoursRemaining - hoursToDisplay) / 24;
+
+  useInterval(
+    () => {
+      if (secondsRemaining > 0) {
+        setSecondsRemaining(secondsRemaining - 1);
+      } else {
+        setStatus(STATUS.STOPPED);
+      }
+    },
+    status === STATUS.STARTED ? 1000 : null
+  );
   return (
     <div className="container mb-5">
       <div className="mt-5">
@@ -36,23 +84,29 @@ function Flash_sale() {
             </div>
             <div className="flex justify-between mt-1">
               <div className="text-base font-semibold">{flashSale.name}</div>
-              <div className="">
-                <span className="bg-black text-white text-xs px-1 py-[2px] rounded-sm">
-                  03
-                </span>
-                :
-                <span className="bg-black text-white text-xs px-1 py-[2px] rounded-sm">
-                  16
-                </span>
-                :
-                <span className="bg-black text-white text-xs px-1 py-[2px] rounded-sm">
-                  27
-                </span>
-                :
-                <span className="bg-black text-white text-xs px-1 py-[2px] rounded-sm">
-                  51
-                </span>
-              </div>
+              {status === STATUS.STARTED ? (
+                <div className="">
+                  <span className="bg-black text-white text-xs px-1 py-[2px] rounded-sm">
+                    {twoDigits(daysToDisplay)}
+                  </span>
+                  :
+                  <span className="bg-black text-white text-xs px-1 py-[2px] rounded-sm">
+                    {twoDigits(hoursToDisplay)}
+                  </span>
+                  :
+                  <span className="bg-black text-white text-xs px-1 py-[2px] rounded-sm">
+                    {twoDigits(minutesToDisplay)}
+                  </span>
+                  :
+                  <span className="bg-black text-white text-xs px-1 py-[2px] rounded-sm">
+                    {twoDigits(secondsToDisplay)}
+                  </span>
+                </div>
+              ) : (
+                <div className="text-center mb-4 text-red-500">
+                  Time is expired
+                </div>
+              )}
             </div>
           </Link>
         ))}
--- pages/flash_sale_detail.jsx
+++ pages/flash_sale_detail.jsx
@@ -1,5 +1,13 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Product from "../components/Product";
+
+const STATUS = {
+  STARTED: "Started",
+  STOPPED: "Stopped",
+};
+
+// const INITIAL_COUNT = 11; // 11sec
+const INITIAL_COUNT = 172811; // 2days 11sec
 
 const products = [
   {
@@ -149,30 +157,91 @@ const products = [
 ];
 
 function FlashSaleDetail() {
-  return (
-    <div className="container mb-5">
-      <div className="mt-5">
-        <img src="/flash_sale_banar.png" />
-        {/* <img src="/flashsale-main.png" /> */}
-      </div>
+  const [secondsRemaining, setSecondsRemaining] = useState(INITIAL_COUNT);
+  const [status, setStatus] = useState(STATUS.STARTED);
 
-      <div className="mt-4">
-        <h3 className="text-center text-2xl">Empower your smartphones</h3>
-        <div className="text-center mb-4 text-xl">
-          <span className="bg-black text-white px-2 py-1 rounded-sm">03</span>:
-          <span className="bg-black text-white px-2 py-1 rounded-sm">16</span>:
-          <span className="bg-black text-white px-2 py-1 rounded-sm">27</span>:
-          <span className="bg-black text-white px-2 py-1 rounded-sm">51</span>
+  const secondsToDisplay = secondsRemaining % 60;
+  const minutesRemaining = (secondsRemaining - secondsToDisplay) / 60;
+  const minutesToDisplay = minutesRemaining % 60;
+  const hoursRemaining = (minutesRemaining - minutesToDisplay) / 60;
+  const hoursToDisplay = hoursRemaining % 24;
+  const daysToDisplay = (hoursRemaining - hoursToDisplay) / 24;
+
+  useInterval(
+    () => {
+      if (secondsRemaining > 0) {
+        setSecondsRemaining(secondsRemaining - 1);
+      } else {
+        setStatus(STATUS.STOPPED);
+      }
+    },
+    status === STATUS.STARTED ? 1000 : null
+  );
+
+  return (
+    <>
+      <div className="container mb-5">
+        <div className="mt-5">
+          <img src="/flash_sale_banar.png" />
+          {/* <img src="/flashsale-main.png" /> */}
+        </div>
+
+        <div className="mt-4">
+          <h3 className="text-center text-2xl">Empower your smartphones</h3>
+
+          {status === STATUS.STARTED ? (
+            <div className="text-center mb-4 text-xl">
+              <span className="bg-black text-white px-2 py-1 rounded-sm">
+                {twoDigits(daysToDisplay)}
+              </span>
+              <span className="text-[#DE146A] font-bold">:</span>
+              <span className="bg-black text-white px-2 py-1 rounded-sm">
+                {twoDigits(hoursToDisplay)}
+              </span>
+              <span className="text-[#DE146A] font-bold">:</span>
+              <span className="bg-black text-white px-2 py-1 rounded-sm">
+                {twoDigits(minutesToDisplay)}
+              </span>
+              <span className="text-[#DE146A] font-bold">:</span>
+              <span className="bg-black text-white px-2 py-1 rounded-sm">
+                {twoDigits(secondsToDisplay)}
+              </span>
+            </div>
+          ) : (
+            <div className="text-center mb-4 text-xl text-red-500">
+              Time is expired
+            </div>
+          )}
+        </div>
+
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 gap-2.5">
+          {products.map((product, index) => (
+            <Product product={product} key={index} />
+          ))}
         </div>
       </div>
-
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 gap-2.5">
-        {products.map((product, index) => (
-          <Product product={product} key={index} />
-        ))}
-      </div>
-    </div>
+    </>
   );
 }
+
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
+
+const twoDigits = (num) => String(num).padStart(2, "0");
 
 export default FlashSaleDetail;
