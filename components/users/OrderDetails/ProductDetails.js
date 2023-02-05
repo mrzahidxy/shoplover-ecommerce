747bf7b66f1ec45cb52d3bf736da46f03c4c37ba
Rakibul Alam Nahin <ranahin648@gmail.com>
2023-02-05 16:14:26 +0600

    order details done

--- components/users/OrderDetails/ProductDetails.js
+++ components/users/OrderDetails/ProductDetails.js
@@ -4,61 +4,62 @@ import React from 'react'
 const ProductDetails = ({ Details, User }) => {
     return (
         <>
-            <div className='sm:max-md:border-2 sm:max-md:pt-3 sm:max-md:font-bold'>
-                <div className='flex items-center ml-6'>
-                    <h1 className=' text-sm sm:max-md:text-lg col-span-3'>Ordered Products</h1>
-                    <h1 className='ml-2 w-5 h-5 flex justify-center items-center rounded-full bg-black text-white'>{Details.products.length}</h1>
+            <div className='sm:max-md:border-2 sm:max-md:pt-3 flex flex-col sm:max-md:font-bold'>
+                <div className='flex flex-row justify-between items-center ml-6 lg:ml-8 w-44 my-3'>
+                    <span className=''>Ordered Products</span>
+                    <span className='w-5 h-5 flex justify-center items-center rounded-full bg-black text-white'>{Details.products.length}</span>
 
                 </div>
 
-                <div className='mt-4 rounded-b-md flex flex-col bg-white'>
+                <div className='rounded-b-md flex flex-col bg-white px-10 '>
+                    <div className='border-b-2 border-dashed max-w-[600px]'>
+                        {
+                            Details.products.map((items, index) => (
+                                <div key={index} className='flex flex-row justify-between'>
 
-                    {
-                        Details.products.map((items, index) => (
-                            <div key={index} className='sm:mx-4 lg:ml-8 px-2 md:grid md:grid-cols-2 sm:flex sm:flex-row  sm:max-md:justify-between'>
-
-                                <div className='flex flex-row my-4 sm:max-md:font-medium'>
-                                    <Image className='w-12 h-12 mr-4' src={items.image} width={40} height={10} alt='' />
-                                    <div>
-                                        <h1 className='flex-col sm:max-md:w-28 sm:max-md:h-5 sm:max-md:overflow-hidden'>{items.name}</h1>
-                                        <h1 className='text-toosm text-mygrey'>Qty: {items.quantity} Color: {items.color}</h1>
+                                    <div className='flex flex-row my-4 sm:max-md:font-medium'>
+                                        <Image className='w-12 h-12 mr-4 border-6 border-black' src={items.image} width={40} height={10} alt='' />
+                                        <div>
+                                            <h1 className='flex-col sm:max-md:w-28 sm:max-md:h-5 sm:max-md:overflow-hidden'>{items.name}</h1>
+                                            <h1 className='text-toosm text-mygrey'>Qty: {items.quantity} Color: {items.color}</h1>
+                                        </div>
                                     </div>
-                                </div>
 
-                                <div className='flex flex-row justify-end my-4 lg:w-64'>
-                                    <h1 className='flex flex-row'>৳{items.price}</h1>
+                                    <div className='flex flex-col justify-center font-bold'>৳{items.price}</div>
 
                                 </div>
+                            ))
+                        }
+                    </div>
 
-                            </div>
-                        ))
-                    }
-                    <div className='sm:mx-4 lg:ml-8 border-t-2 border-dashed border-neutral-300'></div>
-
-                    <div className=' sm:mx-4 lg:ml-8 px-4 grid sm:grid-cols-2 my-2 '>
-
-                        <div className=' flex flex-col'>
-                            <h1 className='my-1'>Sub Total</h1>
-                            <h1 className='my-1'>Tax</h1>
-                            <h1 className='my-1'>Shipping Cost</h1>
-                            <h1 className='my-1'>Discount</h1>
-
-
+                    <div className='border-b-2 border-dashed flex flex-col max-w-[600px]'>
+                        <div className='flex flex-row justify-between my-4 font-medium'>
+                            <span>Sub Total</span>
+                            <span className=''> ৳ {Details.sub_total}</span>
                         </div>
 
-                        <div className='lg:w-64 flex flex-col justify-end'>
-                            <h1 className='my-1 '> ৳ {Details.sub_total}</h1>
-                            <h1 className='my-1 '> ৳ {Details.tax}</h1>
-                            <h1 className='my-1 '> ৳ {Details.shipping_cost}</h1>
-                            <h1 className='my-1 '> ৳ {Details.discount}</h1>
+                        <div className='flex flex-row justify-between my-4 font-medium'>
+                            <span>Tax</span>
+                            <span className=''> ৳ {Details.tax}</span>
                         </div>
+
+                        <div className='flex flex-row justify-between my-4 font-medium'>
+                            <span>Shipping Cost</span>
+                            <span className=''> ৳ {Details.shipping_cost}</span>
+                        </div>
+
+                        <div className='flex flex-row justify-between my-4 font-medium'>
+                            <span>Discount</span>
+                            <span className=''> ৳ {Details.discount}</span>
+                        </div>
+
 
                     </div>
-                    <div className='sm:mx-4 md:ml-8 px-4 border-t-2 border-dashed border-neutral-300'></div>
 
-                    <div className='sm:mx-4 lg:ml-8 px-4 grid md:grid-cols-2 my-6 sm:flex sm:flex-row  sm:max-md:justify-between'>
-                        <h1>Grand Total</h1>
-                        <h1 className='flex flex-row justify-end lg:w-64 '> ৳ {Details.grand_total}</h1>
+
+                    <div className=' my-4 max-w-[600px] flex flex-row justify-between font-bold '>
+                        <span>Grand Total</span>
+                        <span className=''> ৳ {Details.grand_total}</span>
 
                     </div>
 
